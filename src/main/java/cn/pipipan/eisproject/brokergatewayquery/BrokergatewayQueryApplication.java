d4071b99c1ba98bd0d9fc96a33017f9bb799507a
ssingularity <1755405701@qq.com>
2019-05-18 16:56:23 +0800

    initialize

---
+++ src/main/java/cn/pipipan/eisproject/brokergatewayquery/BrokergatewayQueryApplication.java
@@ -0,0 +1,13 @@
+package cn.pipipan.eisproject.brokergatewayquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrokergatewayQueryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrokergatewayQueryApplication.class, args);
+    }
+
+}
