95216cb58b77cdde74c4e7388d357fcda00303e5
Yifan-Song <songyifanbj@163.com>
2019-05-31 00:47:32 +0800

    Add OrderBlotter Query

--- src/main/java/cn/pipipan/eisproject/brokergatewayquery/repository/OrderBlotterDTORepository.java
+++ src/main/java/cn/pipipan/eisproject/brokergatewayquery/repository/OrderBlotterDTORepository.java
@@ -3,7 +3,11 @@ package cn.pipipan.eisproject.brokergatewayquery.repository;
 import cn.pipipan.eisproject.brokergatewayquery.domain.OrderBlotter;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import java.util.*;
 
 @Repository
 public interface OrderBlotterDTORepository extends MongoRepository<OrderBlotter, String> {
+    
+    public List<OrderBlotter> findByMarketDepthId(String MarketDepthId);
+
 }
